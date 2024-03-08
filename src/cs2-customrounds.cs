deeef233543bf5cf4ff5ff281b6302d64f99f5d1
schwarper <75811921+schwarper@users.noreply.github.com>
2024-03-08 22:20:06 +0300

    0.0.7

--- src/cs2-customrounds.cs
+++ src/cs2-customrounds.cs
@@ -2,15 +2,20 @@ using CounterStrikeSharp.API.Core;
 
 namespace CustomRounds;
 
-public partial class CustomRounds : BasePlugin
+public partial class CustomRounds : BasePlugin, IPluginConfig<CustomRoundsConfig>
 {
     public override string ModuleName => "Custom Rounds";
-    public override string ModuleVersion => "0.0.6";
+    public override string ModuleVersion => "0.0.7";
     public override string ModuleAuthor => "schwarper";
 
     public override void Load(bool hotReload)
     {
         LoadCommands();
         LoadEvents();
+    }
+
+    public void OnConfigParsed(CustomRoundsConfig config)
+    {
+        Config = config;
     }
 }
